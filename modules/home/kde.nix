b10d773476cbc3bb57d808fb2ce7aca64705cd5d
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2023-03-28 07:31:15 +0800

    Randomize Breeze color

--- modules/home/kde.nix
+++ modules/home/kde.nix
@@ -10,4 +10,58 @@
     krdc
     libsForQt5.dolphin
   ];
+
+
+  xdg.dataFile."konsole/Breeze.colorscheme" = {
+    source =
+      let
+        concatString = pkgs.lib.concatStringsSep "\n";
+        sections = [
+          "Background"
+          "BackgroundFaint"
+          "BackgroundIntense"
+          "Foreground"
+          "ForegroundFaint"
+          "ForegroundIntense"
+        ];
+        appendRandom = {
+          RandomHueRange = 360;
+          RandomSaturationRange = 100;
+          # RandomLightnessRange = 10;
+        };
+        general =
+          {
+            Blur = true;
+            ColorRandomization = true;
+            Opacity = 0.83;
+            Description = "Breeze Randomized";
+            FillStyle = "Title";
+          };
+        breeze = pkgs.runCommand "my-derivation" { } ''
+            install -Dm644 "${pkgs.konsole}/share/konsole/Breeze.colorscheme" $out
+
+            # Remove all keys of appendRandom
+            ${concatString (
+              pkgs.lib.mapAttrsToList (k: v: "sed -i '/${k}=/d' $out") appendRandom
+            )}
+
+            # For each section append the new key
+            for section in ${pkgs.lib.concatStringsSep " " sections}; do
+              ${concatString (
+                pkgs.lib.mapAttrsToList (k: v: "sed -i '/\\['$section'\\]/a ${k}=${toString v}' $out") appendRandom
+              )}
+            done          
+
+            # Remove the full [General] section
+            sed -i '/\[General\]/,/\[/d' $out
+            # Append the new [General] section
+            echo "[General]" >> $out
+            ${concatString (
+              pkgs.lib.mapAttrsToList (k: v: "echo '${k}=${toString v}' >> $out") general
+            )}
+        '';
+      in
+        breeze.out;
+  };
+
 }
