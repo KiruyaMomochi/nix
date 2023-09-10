2798f9a937f9669675f3edd513d5a3f592ef7c05
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2023-09-11 01:10:19 +0800

    MongoDB and (not work yet) InfluxDB 2

--- hosts/nightmare/default.nix
+++ hosts/nightmare/default.nix
@@ -62,6 +62,11 @@
   networking.firewall.allowedTCPPorts = [ 22 80 443 ];
   networking.firewall.allowedUDPPorts = [ 443 ];
 
+  # MongoDB
+  services.mongodb = {
+    enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
