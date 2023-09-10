725d5d7574dcd80f2a4072469b6532a3232019bb
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2023-09-10 08:10:03 +0800

    Add carmina

---
+++ hosts/carmina/secret.nix
@@ -0,0 +1,24 @@
+{
+	"data": "ENC[AES256_GCM,data:YuRuSnzDJpjXIQW3xgg8PYXt/flBS6zeQuelWPkKhS4oPMdhJaIdLiDjnvuPJxFjwC81+cctTLv0hTUmgIl33s0/+SvcUEvoZ+3IHJ7+ePII9k8rdTn9KH49AmXvya9XsUHraOKYACQyOpVygBTy9ocTIv17BROjDTw8Q1h/rkwWR/mErhrsD4EmRSyQi5ZYpauuD1VfrfRciuWAO+56gApfAq7234kza8cTWaVvJItOw1eHrsmVrGyDg8lfLQjiHRS9qbMxjIJbiousxwOIbDEjTQRH3EoDTtOleXqLeFzB8lcn0f/MjfPkgbW1yp2c5VDSSv8GmFKBJStEHhebs1+OenGq1O+BAZQFMYLIOFShDhzW2+x3lY+rNCeF75Hg9S4RI8vsohEZsQIHV7LQElvTNs7UDdcPqw5SzByyL1ztKhhKkzhsFtgo2YsGEqDS+6QmwCMKADUcZLSZUhtENeyhCmhHG/KrWa795BDqsNTsqCZqmDeqxmcLO0eYpvpr8XV8vWKGWntPrlpUUIbDIzQbboT8viaCQiLmF8ud65QVZvQhQGAN9E98zWURhmHwggM2RNfqPYjaIMlu8IiGUcbBv1fjSKa4ii4QhjtoIeHegFZzCEeqoHbyjitTJJIa6mmEglAHlKjM1XaVWx/dPaitU9OS9FuXCHZS8bCC3BKoiJk4GDmi+woL6nsqKrdnM5hw0Wp99ewOQN8iKhvyTmHjbGrNnpXcrHw/JnLxXxj50HWUdUnRsDPWkfuOsH95xWYCdgaoqlLsr/yd6ukV6eQPraoYfhI2RcdIv9s74lG3B/LG8wzwWF/e9j+5hubsJ+TMfW9H3Ogyb7YyXtNORqMEPYv/88Jq/39ni5tt+JtrE/p9uTCTUql5n3FbSGmRKMpPmRCrLOYE6ekBMI44aPC+glHFIzJniyS4uKn9z8zUYndwDzX8imn10Al3AwhzJWmnWLgvcrbReFu5r6YacyCS+X89A8MAE68KpZ6290u1oo7DKOwlNHSz4BTdaqeTy3Q9eXBJjE7axn2klSGFYA+ci4H1UCrzlwacyh6/ZQKLmRa+tbBiPn2bwW7cbL0nRRovGQdUBcPOpeqLwtPBaDozciICHGF+KDef/ZwyFVSoSKqLtEPCsJdpz/5S5u9UUvw=,iv:R6BSamNk2qIl6k7gibx7im+TPkS28exj2f9X423RfkU=,tag:ikWiEBHhFJng5SYC88Pn7w==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": [
+			{
+				"recipient": "age1zke99l06jf9xc9jjcclncr7fcl4fekrrtza3urt3z3wkawnhr42srgfn6l",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBzMDVIanNpbExLYnJVeE9Q\nNjBwSkJ4Z0c5eU8rVkp1THowdUFlNVZwWGh3CjNyRWtBV09LSTNEcnV3cEw3NU5w\nc1FjaElBc2pKRmMxN2krQTNGV0FaSTQKLS0tIEN3L1cxUkg0RGMzSThTTGh0aG43\nUG80TFpCRDljQWFmQzYyTXRiRis5TVkKt0Kqwt7KwD6SHFrLPx3iXpPac7xasA66\nHdIOHMmiyQi0HVyT3A0McN7opxzJJWOhce2rAZmcFNpSIuvssat29Q==\n-----END AGE ENCRYPTED FILE-----\n"
+			},
+			{
+				"recipient": "age13xy60eaa0nvgjhrlzlq3uggqkydw0ffyjmg9teezwy8a0n2g39usm0rkfz",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBKKzB0ZHNyZnVrTGhabGVU\nWVFLOGZiZUczUDVtdTAwMml3R0w0OXJBZVZ3CkZKeWd4em0zbS9IM0hXMC8vdGlR\nbmVUS1ZVSGFpbG8zU2toWWVucEhBN3cKLS0tIERMbVRndUVBUHBMT0U4MG56dnMy\nRkNsUk45SVFTUld0VlZPODZScy82cHMKEh8g+NKDCxdWVJN6Q9kTnvNK9tpE7+Qu\n21CpBmFWeXza0r9A+vWAzTaFgwFORwUkwLQRl0WcmgnuC21g9ZoHUg==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2023-09-10T00:09:52Z",
+		"mac": "ENC[AES256_GCM,data:wMUGjJCKky2TSqq5Ni9/p1KSFi/IFKNpMbByl522sP4moN6Kyhp5xCnWc+MOu2t0LDw+4rmtP3tPOUjrqQWDNFHGmHQNUNwPyuqwJ1QHqu5lDc3ttoYXwk+WOIQcDIq7l4T0Ol6J7gK//zNkHotr4hWFCJ9Vl3aWk6cuj+Nack8=,iv:WYf2WjiUQOZ82lnVxlQs4Dgs9toDk9IacIH27O46HJQ=,tag:5/UuwBWRkTmS9L5CTEL+og==,type:str]",
+		"pgp": null,
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.7.3"
+	}
+}
