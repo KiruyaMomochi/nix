ca0e4755b6ef4c17c443c71ab311bfd69d52a4e1
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2023-10-03 02:56:44 +0800

    Update sops to 3.8.0

--- packages/sops/default.nix
+++ packages/sops/default.nix
@@ -1,14 +1,14 @@
 { sops, fetchFromGitHub, buildGoModule }:
 let
   attrs = {
-    version = "v3.8.0-rc-lookupconfig-20230909";
+    version = "v3.8.0-lookupconfig-20231003";
     src = fetchFromGitHub {
       rev = "path";
       owner = "KiruyaMomochi";
       repo = "sops";
-      sha256 = "sha256-i3XlQeZpRm8EoUgrnCKiiPn7Rvs+wIdFtpeAi68YMQ0=";
+      sha256 = "sha256-1XmvHSrmtalyk15NkcAsiENZKF8blBSz7GZKxVDffbA=";
     };
-    vendorHash = "sha256-FsmM1zIsB6BXMwMivvjGU/ISayrYyV1M9lU6LUE9dWE=";
+    vendorHash = "sha256-/fh6pQ7u1icIYGM4gJHXyDNQlAbLnVluw5icovBMZ5k=";
   };
 in
 sops.override {
