bda61f003f4a961cf1f7c23cfb7dc6f63b305fb4
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2024-01-07 16:18:18 +0800

    Inherit nixpkgs to registry

--- hosts/default.nix
+++ hosts/default.nix
@@ -118,6 +118,16 @@ in
     environmentFiles = [ config.sops.secrets."influxdb".path ];
   };
 
+  # from https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry
+  # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
+
+  # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
+  # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
+  environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
+  # https://github.com/NixOS/nix/issues/9574
+  nix.settings.nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
+
   # Secrets
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.defaultSopsFile = ./secrets/secrets.yaml;
