f393c44b82a6ee6684f50f481de8f5eef8dd10b4
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2024-05-04 12:49:03 +0800

    Replace previous NIX_PATH setting with the official one

--- hosts/default.nix
+++ hosts/default.nix
@@ -127,9 +127,7 @@ in
 
   # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
   # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
-  environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
-  # https://github.com/NixOS/nix/issues/9574
-  nix.settings.nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
+  nixpkgs.flake.setNixPath = true;
 
   i18n.defaultLocale = mkDefault "en_US.UTF-8";
 
