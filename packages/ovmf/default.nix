ba375184e715514d194227bdcf662ced725b9a5e
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2024-05-04 12:13:00 +0800

    Disable SMM so OVMF works in i440fx

--- packages/ovmf/default.nix
+++ packages/ovmf/default.nix
@@ -7,4 +7,8 @@ in
 (OVMFFull.override
 {
   inherit edk2;
+  # Disable systemManagementModeRequired as legacy gvt-d require i440fx
+  # However, i440fx does not support SMM
+  # This may affect S3 suspend, but it's not a big issue for our virtual environment
+  systemManagementModeRequired = false;
 })
