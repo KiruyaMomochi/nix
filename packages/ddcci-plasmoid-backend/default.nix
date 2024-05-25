561b42b19d0cd620ebb958b9b93829bb46a0d2e4
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2024-05-26 07:23:55 +0800

    Update ddcci-plasmoid

--- packages/ddcci-plasmoid-backend/default.nix
+++ packages/ddcci-plasmoid-backend/default.nix
@@ -4,27 +4,33 @@
 }:
 python3.pkgs.buildPythonApplication rec {
   pname = "ddcci-plasmoid-backend";
-  version = "0.1.8";
+  version = "0.1.10";
   format = "pyproject";
 
   src = fetchPypi {
     pname = "ddcci_plasmoid_backend";
     inherit version;
-    hash = "sha256-tA2eg8iknuFBHsj4HLXyQBrEK5LSTF0z2hRx2lXl6CE=";
+    hash = "sha256-95dGkZqlaxwBiy7LXjTW83D8qVUEFUL5Nely9lzMAzo=";
   };
 
   nativeBuildInputs = with python3.pkgs; [
     poetry-core
+    pythonRelaxDepsHook
     # pytestCheckHook
   ];
 
-  propagatedBuiltInputs = with python3.pkgs; [
-    tox
+  pythonRelaxDeps = [
+    "fasteners"
   ];
 
-  doCheck = false; # skip check because backend/fixtures/basic/indented.txt not included in pypi source package
+  propagatedBuildInputs = with python3.pkgs; [
+    fasteners
+  ];
+
+  doCheck = true; # fasteners<0.19,>=0.18 not satisfied by version 0.19
   nativeCheckInputs = with python3.pkgs; [
     pytest-asyncio
+    tox
   ];
 
   pythonImportsCheck = [
--- packages/ddcci-plasmoid/default.nix
+++ packages/ddcci-plasmoid/default.nix
@@ -6,8 +6,8 @@ stdenvNoCC.mkDerivation {
   src = fetchFromGitHub {
     owner = "davidhi7";
     repo = "ddcci-plasmoid";
-    rev = "1dc2f5594f2cf44496855b169fd1e1a4f1f41546";
-    hash = "sha256-Q0Bbugh1rVl8Ze0yrVMEiQUYbMThj8bYFqzAZ400Yd4=";
+    rev = "db3ca1ac83b9b40c30723555674ca57ae5a0dd61";
+    hash = "sha256-/YzIfkAlbxxS41VGwQ7Br+rGM8T3ez5nVxP536o7Lnk=";
   };
 
   installPhase = ''
