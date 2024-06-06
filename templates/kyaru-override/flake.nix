4f62961e9be779b0f369c35524c46bef67f2efee
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2024-06-06 12:06:46 +0800

    Add override template

---
+++ templates/kyaru-override/flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Extended flake from kyaru";
+
+  inputs = {
+    nix-kyaru.url = "github:KiruyaMomochi/nix";
+    nixpkgs.follows = "nix-kyaru/nixpkgs";
+  };
+
+  outputs = { self, nix-kyaru, nixpkgs }:
+    let
+      inherit (lib.attrsets) mapAttrs recursiveUpdate;
+      inherit (lib.lists) foldl;
+      inherit (lib.kyaru.modules) mapModulesRecursive;
+
+      mkPkgs = pkgs: system: import pkgs {
+        inherit system;
+        config = import ./nixpkgs-config.nix;
+      };
+      lib = nix-kyaru.lib;
+      recursiveUpdateAll = foldl recursiveUpdate { };
+    in
+    nix-kyaru.outputs // {
+      # https://determinate.systems/posts/extending-nixos-configurations
+      nixosConfigurations =
+        let
+          hosts = mapModulesRecursive ./hosts import;
+        in
+        mapAttrs
+          (
+            name: value: value.extendModules {
+              modules = [
+                # { nixpkgs.pkgs = mkPkgs nixpkgs value.pkgs.system; }
+                hosts.${name}
+              ];
+            }
+          )
+          nix-kyaru.outputs.nixosConfigurations;
+
+      homeConfigurations =
+        mapAttrs
+          (
+            name: value: value.override (super: {
+              pkgs = mkPkgs nixpkgs super.pkgs.system;
+              modules = super.modules ++ [
+                ./home.nix
+              ];
+            })
+          )
+          nix-kyaru.outputs.homeConfigurations;
+
+      deploy.nodes =
+        recursiveUpdateAll [
+          nix-kyaru.deploy.nodes
+          (
+            builtins.mapAttrs
+              (name: value:
+                let system = value.pkgs.system; in recursiveUpdateAll [
+                  # Override the previous nixos
+                  { profiles.system.path = self.deployPkgs.${system}.deploy-rs.lib.activate.nixos value; }
+                ])
+              self.nixosConfigurations
+          )
+        ];
+      # https://github.com/serokell/deploy-rs/issues/78 password based sudo
+      deploy.magicRollback = false;
+    };
+}
