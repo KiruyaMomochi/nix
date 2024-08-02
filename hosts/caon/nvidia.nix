c51d9c95294d767c7c883012b36c36c867c22e1b
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2024-08-03 04:02:09 +0800

    Use open source driver

--- hosts/caon/nvidia.nix
+++ hosts/caon/nvidia.nix
@@ -27,14 +27,14 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   security.pam.loginLimits = [{
