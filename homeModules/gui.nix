84ae3147bf2e3a26981047e13364ccad30afb647
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2024-11-03 14:12:51 +0800

    Use upstream goldendict-ng

--- homeModules/gui.nix
+++ homeModules/gui.nix
@@ -26,7 +26,7 @@ in
       (google-chrome.override {
         commandLineArgs = "--enable-features=WebUIDarkMode --force-dark-mode --disable-features=UserAgentClientHint";
       })
-      kyaru.qt5.goldendict-ng
+      goldendict-ng
       ddcui
       drawio
       remmina
