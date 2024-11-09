8a35de3c88789122e223284f26fc1b655075b10a
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2024-11-09 12:14:14 +0800

    Add HedgeDoc to white-wings

--- nixosConfigurations/white-wings/default.nix
+++ nixosConfigurations/white-wings/default.nix
@@ -10,6 +10,10 @@
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
   time.timeZone = "America/Los_Angeles";
 
+  services.hedgedoc = {
+    enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
