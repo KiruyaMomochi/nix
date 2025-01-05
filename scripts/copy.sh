22d00ac59ff766c9528385a0c814ed6420b4c477
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2025-01-05 16:22:07 +0800

    Add copy scripts

---
+++ scripts/copy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -x
+
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+mapfile -t targets < "$SCRIPT_DIR/targets.txt"
+nix copy --to 's3://nix-cache?scheme=https&endpoint=usc1.contabostorage.com&secret-key='$(realpath ~/.config/nix/secret-key) "${targets[@]}"
