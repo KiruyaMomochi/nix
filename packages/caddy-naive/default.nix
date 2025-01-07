9ffc72cd360dabf97b756ee1658b01fecf68e0a9
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2025-01-08 00:09:27 +0800

    Fix update script and update hash

--- packages/caddy-naive/default.nix
+++ packages/caddy-naive/default.nix
@@ -4,7 +4,7 @@
 caddy.override {
   buildGoModule = args: buildGoModule (args // {
     pname = "caddy-naive";
-    vendorHash = "sha256-yP8Y1HORdwIxWbcL4K0PTt6Lw5eDuWZLfRMr3LBFnqM=";
+    vendorHash = "sha256-L9Q1+duN4TrFba7jL1A7nmr6gN69en3mjz9wqF+0+po=";
 
     prePatch = ''
       sed -i -e '\!// plug in Caddy modules here!a _ "github.com/caddyserver/forwardproxy"' ./cmd/caddy/main.go
--- packages/caddy-naive/update.sh
+++ packages/caddy-naive/update.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env nix
-#!nix shell nixpkgs#bash nixpkgs#go nixpkgs#nix-prefetch --command bash
+#!nix shell nixpkgs#bash nixpkgs#go nixpkgs#nurl --command bash
 # shellcheck shell=bash
 
 set -xeuo pipefail
 
 tempdir=$(mktemp -d --suffix caddy-naive)
-src=$(nix build nixpkgs#caddy.src --no-link --print-out-paths)
+src=$(nix build github:nixos/nixpkgs#caddy.src --no-link --print-out-paths)
 
 pushd "$tempdir" || exit
 cp -r --no-preserve=mode "$src/." .
 sed -i -e '\!// plug in Caddy modules here!a _ "github.com/caddyserver/forwardproxy"' ./cmd/caddy/main.go
 go mod edit -replace github.com/caddyserver/forwardproxy=github.com/klzgrad/forwardproxy@naive
-go mod tidy
+go mod tidy -e
 go mod vendor
 popd || exit
 
 cp -r "$tempdir/go.mod" "$tempdir/go.sum" .
 
-oldHash=$(nix eval --impure --raw --expr '((import <nixpkgs> {}).callPackage ./default.nix {}).vendorHash')
-newHash=$(nix-prefetch --option extra-experimental-features 'nix-command flakes' -v '{ sha256 }: (callPackage (import ./default.nix) { } ).goModules.overrideAttrs (_: { outputHash = sha256; })')
+# nix build .#caddy-naive --no-link -L
+
+nixpkgs=$(nix eval -I "nixpkgs=flake:github:nixos/nixpkgs" --expr '<nixpkgs>' --impure)
+oldHash=$(nix eval --impure --raw --expr "((import ${nixpkgs} {}).callPackage ./default.nix {}).vendorHash")
+newHash=$(nurl -e "((import ${nixpkgs} {}).callPackage (import ./default.nix) { } ).goModules")
 echo "${oldHash} -> ${newHash}"
 sed -i "s|vendorHash = \"${oldHash}\"|vendorHash = \"${newHash}\"|" ./default.nix
