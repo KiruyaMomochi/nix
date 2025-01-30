10e732cc40e23f5ba13252c206de49346d4efe2c
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2025-01-31 05:03:31 +0800

    ci: add actions

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -4,5 +4,5 @@ set -x
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 mapfile -t targets < "$SCRIPT_DIR/targets.txt"
 for target in "${targets[@]}"; do
-  nix copy --print-build-logs --verbose --to 's3://nix-cache?scheme=https&endpoint=usc1.contabostorage.com&secret-key='$(realpath ~/.config/nix/secret-key) "$target"
+  nix copy --print-build-logs --to 's3://nix-cache?scheme=https&endpoint=usc1.contabostorage.com&secret-key='$(realpath ~/.config/nix/secret-key) "$target"
 done
