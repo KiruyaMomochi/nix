367ebfc6b5bb7d3c9f1ac332af2acff7afea22cd
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2025-01-31 11:55:43 +0800

    feat: add groups

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -4,5 +4,7 @@ set -x
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 mapfile -t targets < "$SCRIPT_DIR/targets.txt"
 for target in "${targets[@]}"; do
+  echo "::group::$target"
   nix copy --print-build-logs --to 's3://nix-cache?scheme=https&endpoint=usc1.contabostorage.com&secret-key='$(realpath ~/.config/nix/secret-key) "$target"
+  echo "::endgroup::"
 done
