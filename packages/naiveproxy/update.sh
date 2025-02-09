12a4edd81fac084959265a7f92c818c410ee0f00
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2025-02-10 04:47:11 +0800

    naiveproxy: update

--- packages/naiveproxy/update.sh
+++ packages/naiveproxy/update.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p nix-prefetch -p curl -p jq
+#!nix-shell -i bash -p nurl -p curl -p jq
 # shellcheck shell=bash
 
 set -exuo pipefail
 
 latest_tag=$(curl https://api.github.com/repos/klzgrad/naiveproxy/releases/latest | jq --raw-output .tag_name)
-latest_hash=$(nix-prefetch fetchFromGitHub --owner "klzgrad" --repo "naiveproxy" --rev "$latest_tag")
+latest_hash=$(nurl --hash https://github.com/klzgrad/naiveproxy "$latest_tag")
 
 sed -i "/^  version = .*;/c\  version = \"${latest_tag#v}\";" ./default.nix
 sed -i "/^    sha256 = .*;/c\    sha256 = \"${latest_hash}\";" ./default.nix
