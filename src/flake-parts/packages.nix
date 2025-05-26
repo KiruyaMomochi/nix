19e9286a359ada0bd4c642cefc66703117e507bc
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2025-05-27 05:36:40 +0800

    fix: python3Packages.rapidocr-onnxruntime: rm broken tests with cudaSupport

--- src/flake-parts/packages.nix
+++ src/flake-parts/packages.nix
@@ -58,7 +58,16 @@ in
         });
         pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
           (
-            python-final: python-prev: { }
+            python-final: python-prev: {
+              # Remove after https://github.com/NixOS/nixpkgs/pull/382920 is merged
+              rapidocr-onnxruntime = python-prev.rapidocr-onnxruntime.overridePythonAttrs (old: {
+                disabledTests = final.lib.throwIf (builtins.elem "test_ort_dml_warning" old.disabledTests) "test_ort_dml_warning is already in disabledTests, remove the patch!" (old.disabledTests ++ final.lib.optionals final.onnxruntime.cudaSupport [
+                  # segfault when built with cuda support but GPU is not availaible in build environment
+                  "test_ort_cuda_warning"
+                  "test_ort_dml_warning"
+                ]);
+              });
+            }
           )
         ];
       });
