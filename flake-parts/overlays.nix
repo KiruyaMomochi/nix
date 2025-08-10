7a8f8f10dcda888d751b8714afb99f67f46b1b42
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2025-08-10 14:58:53 +0800

    openobserve: fix cannot coerce a list to a string

--- flake-parts/overlays.nix
+++ flake-parts/overlays.nix
@@ -27,7 +27,7 @@ in
           "--skip=service::db::user::tests::test_user"
           "--skip=service::metadata::trace_list_index::tests::test_write"
         ];
-        patches = (oldAttrs.patches or [ ] ) + [
+        patches = (oldAttrs.patches or [ ] ) ++ [
           ../packages/openobserve.patch
         ];
       });
