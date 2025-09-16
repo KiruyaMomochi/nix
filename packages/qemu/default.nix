617e0ce54646e9ac19daaadc926417444d5cfd70
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2025-09-16 23:15:45 +0800

    qemu: x-pci-class-code has already been added in upstream
    https://github.com/qemu/qemu/commit/1b50621881241ac5bc75ae7f8aa4c278ada8a668

--- packages/qemu/default.nix
+++ packages/qemu/default.nix
@@ -3,7 +3,6 @@
 qemu.overrideAttrs (oldAttrs: {
   patches = oldAttrs.patches ++ [
     # https://listman.redhat.com/archives/vfio-users/2019-February/004819.html
-    ./spoof-pci-class-code.patch
     ./disable-igd-vga-check.patch
   ];
 })
