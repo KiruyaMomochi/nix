c43eb795f2981b83236114fac7bc7c2b624ac805
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2026-01-23 03:14:51 +0800

    update: depenedencies

--- packages/gemini-cli-preview/default.nix
+++ packages/gemini-cli-preview/default.nix
@@ -4,10 +4,10 @@
 , ripgrep
 }:
 gemini-cli-bin.overrideAttrs (finalAttrs: oldAttrs: {
-  version = "0.26.0-preview.1";
+  version = "0.27.0-nightly.20260122.61040d0eb";
   src = fetchurl {
     url = "https://github.com/google-gemini/gemini-cli/releases/download/v${finalAttrs.version}/gemini.js";
-    hash = "sha256-dgaQQ3N3phS18JYpccVe6V+dtV1FKW/BB0IWOuqItHo=";
+    hash = "sha256-74DoQLBPDawMmUv3vFKUAh/qhtbdBA7xhqICcjSyxtQ=";
   };
   installPhase = ''
     runHook preInstall
