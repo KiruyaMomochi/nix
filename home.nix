af0cd0e4298e01df6dafc944517927ddb51e9577
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2026-06-09 10:46:45 +0800

    fix: maybe we still need to add channel

--- home.nix
+++ home.nix
@@ -29,6 +29,7 @@
     ];
   };
   nix.package = pkgs.nixVersions.latest;
+  nix.channels.home-manager = inputs.home-manager;
 
   # Packages that should be installed to the user profile.
   home.packages =
