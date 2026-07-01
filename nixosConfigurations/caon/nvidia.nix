bd78254835b25e06ceffe8ebd34cd97c91bcf18c
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2026-07-02 03:39:50 +0800

    fix(caon): use latest nvidia driver

--- nixosConfigurations/caon/nvidia.nix
+++ nixosConfigurations/caon/nvidia.nix
@@ -47,7 +47,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   virtualisation.containers.containersConf.settings.engine.cdi_spec_dirs = [
