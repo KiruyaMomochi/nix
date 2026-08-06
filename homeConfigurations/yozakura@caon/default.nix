385889107f3282710cc7227720589fd6d4678df9
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2026-08-07 02:58:40 +0800

    home: force headless yozakura overrides

--- homeConfigurations/yozakura@caon/default.nix
+++ homeConfigurations/yozakura@caon/default.nix
@@ -1,8 +1,8 @@
-{ ... }:
+{ lib, ... }:
 {
   home.username = "yozakura";
-  programs.kyaru.desktop.enable = false;
-  programs.kyaru.kde.enable = false;
-  services.vscode-server.enable = false;
+  programs.kyaru.desktop.enable = lib.mkForce false;
+  programs.kyaru.kde.enable = lib.mkForce false;
+  services.vscode-server.enable = lib.mkForce false;
   services.cdp-proxy.enable = true;
 }
